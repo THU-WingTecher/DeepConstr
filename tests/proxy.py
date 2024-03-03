f8a5abad3bd38b41197027c2c3467fb46ac0da2d
GwiHwan-Go <iejw1914@gmail.com>
2024-03-03 15:34:25 +0000

    update trivial change

--- tests/proxy.py
+++ tests/proxy.py
@@ -4,6 +4,13 @@
 import os
 from dotenv import load_dotenv
 
+def proxy_check(cfg) : 
+    from neuri.constrinf.inferencer import Inferencer
+    inferencer = Inferencer(cfg['llm']['settings'])
+    inferencer.change_to_gpt3()
+    prompts = 'hello'
+    results = inferencer.inference(prompts)
+    print(results)
 
 def check_internet_connection(url='http://www.baidu.com'):
     import requests
@@ -15,24 +22,25 @@ def check_internet_connection(url='http://www.baidu.com'):
     except requests.RequestException as e:
         print("Internet is not connected:", e)
 
-def proxy_check(cfg) : 
-    import openai
-    load_dotenv(override=True)
-    os.environ['ALL_PROXY'] =os.getenv("MYPROXY")
-    openai.api_key = os.getenv('OPENAI_API_KEY1')
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {'role' : "system", 'content' : "hi"},
-            {'role': 'user', 'content': "hi"},
-        ],
-        )
-    print(completion.choices[0].message['content'])
-    return completion
+# def proxy_check(cfg) : 
+#     import openai
+#     openai.api_key = os.getenv('OPENAI_API_KEY1')
+#     completion = openai.ChatCompletion.create(
+#         model="gpt-3.5-turbo",
+#         messages=[
+#             {'role' : "system", 'content' : "hi"},
+#             {'role': 'user', 'content': "hi"},
+#         ],
+#         )
+#     print(completion.choices[0].message['content'])
+#     return completion
 import hydra
 from omegaconf import DictConfig
 @hydra.main(version_base=None, config_path="../neuri/config", config_name="main")
 def main(cfg: DictConfig):
+    load_dotenv(override=True)
+    os.environ['ALL_PROXY'] =os.getenv("MYPROXY")
+    check_internet_connection("http://www.google.com")
     proxy_check(cfg)
 
 if __name__ == "__main__" :
